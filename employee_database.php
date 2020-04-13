cbc1987bfc7fb70a92c19c9405a6a8f00d273dad
Madhav00786 <46637049+Madhav00786@users.noreply.github.com>
2020-04-13 13:30:51 -0400

    Add files via upload

--- employee_database.php
+++ employee_database.php
@@ -10,8 +10,8 @@
 
 
     $DB_HOST = "localhost";
-    $DB_USER = "Madhav	";
-    $DB_PASSWORD = "yA956952";
+    $DB_USER = "root";
+    $DB_PASSWORD = "";
     $DB_DATABASE = "nylene";
 
     $connect = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
