a79df23297f188f6cc0935fef59e4d9bf469f5aa
waid0005 <waid0005@algonquinlive.com>
2020-04-14 15:51:35 -0400

    Update AuthenticateUser.php
    added a few lines of logic for a basic string compare for our basic admin accounts we created, before hashing was implemented

--- AuthenticateUser.php
+++ AuthenticateUser.php
@@ -28,7 +28,16 @@ session_start();
 	    
 	    echo "<meta http-equiv = \"refresh\" content = \"0; url = ./Homepage.php\" />;";
 	    exit();
-	} else {
+	    
+	    //Added this if so we can still use our admin accounts that were originally created
+	} else if(!strcmp($password1, $password2)){
+	    $_SESSION['name'] = $row['first_name']." ".$row['last_name'];
+	    $_SESSION['role'] = $row['title'];
+	    $_SESSION['userid'] = $row['employee_id'];
+	    
+	    echo "<meta http-equiv = \"refresh\" content = \"0; url = ./Homepage.php\" />;";
+	    exit();
+	}else {
 	    echo "<meta http-equiv = \"refresh\" content = \"0; url = ./login.php\" />;";
 	    exit();
 	}
