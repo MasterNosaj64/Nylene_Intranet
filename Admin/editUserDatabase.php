a83b8cf577e4829f8e2a028b6a2950aad35a30eb
Madhav00786 <46637049+Madhav00786@users.noreply.github.com>
2020-10-01 09:40:04 -0400

    Add files via upload

--- Admin/editUserDatabase.php
+++ Admin/editUserDatabase.php
@@ -1,6 +1,5 @@
 
 <?php
-//session_start();
  if (!session_id()) {
 session_start();
 unset($_SESSION['company_id']);
@@ -12,85 +11,175 @@ include '../Database/connect.php';
 
 } 
 
-//print_r();
 
 ?>
+<!DOCTYPE html>
 
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
 <link rel="stylesheet" href="../CSS/table.css">
 </head>
-<form method="post" action="" >
+<!--<form autocomplete="off" method="post" action="" >-->
 
-<tr>
-<!--<form method="post" action="" >
-  <label for="subjectEmail"><b>
-	Enter employee email:</b></label><br />
-	<input name="subjectEmail" type="email" maxlength="50" />
-    
-<input type="submit" name="Submit">-->
+<body>
 
-<td>Name of the user to be edited <select id="employee" name="edit_user">
-		<?php
-//$connect = mysqli_connect("localhost", "root", "");
-//$db = mysqli_select_db($connect, "nylene");
+
+<form autocomplete="off"  action="/editUserDatabase.php" >
+  <tr><h2>Name of the user to be edited</h2>
+</tr><tr>
+  <div class="autocomplete" style="width:300px;">
+ <input id="myInput"  name="edit_user" >
+</tr>
+
+
+<?php
+
 $sql = "SELECT * FROM employee";
-$query = mysqli_query($connect, $sql);
+$query = mysqli_query($conn, $sql);
+$value=array();
 while ($row = mysqli_fetch_array($query)) {
-    echo '<option style="width: 260px" value=' . $row['employee_id'] . '>' . $row['first_name'] . " " . $row['last_name'] . '</option>';
-}
-?>
-		</select></td>
+//    echo '<option style="width: 260px" value=' . $row['employee_id'] . '>' . $row['first_name'] . " " . $row['last_name'] . '</option>';
+    $value[] =  $row['first_name'] . " " . $row['last_name'];
+    //echo $row["first_name"];
 
-  </tr>
-  
+}
+?> 
+
+
+</div>
+		
   <input type="submit" name="Submit">
 			<input type="reset" name="Reset">
 		
 </form>
+
+
+<script>
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+
+var countries = <?php echo json_encode($value); ?>
+
+//var countries =[];
+// <?php echo json_encode($value); ?>
+//countries.push('		
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+autocomplete(document.getElementById("myInput"), countries);
+</script>
 
 </html>
 
 
 <?php 
 if(isset($_POST['Submit'] )){
-    /*$DB_HOST = "localhost";
-    $DB_USER = "root";
-    $DB_PASSWORD = "";
-    $DB_DATABASE = "nylene";
-    $connect = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
-	$db=mysqli_select_db($connect,"nylene");
-*/
+   
 	$field=$_POST['edit_user'];
 			$_SESSION['field'] = $field;
 
-			//echo '<h1>MySQL Server is connected</h1>';
+
 			header('location:editUser.php');
 
-	//$check=0;
-
-   
-
-
-	//if($connect) {
-  
-		   
-		//$sql = "SELECT employee_id FROM employee";
-		//$query = mysqli_query($connect,$sql);
-		/**while ($row = mysqli_fetch_array($query)) {
-			if($field==$row['employee_email']){
-				$check=1;
-			}
-		}			
-			
-		if($check==1){
-		*/
-		//$_SESSION['field'] = $field;
-			//header('location:editUser.php');
-		//}
-		//else{
-			//echo 'No such email found';
-		//}
 	}
 	
 	
