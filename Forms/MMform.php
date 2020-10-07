ba6c2c9991ab5783e6f99e021872f7d8e5014a6c
Karandeep1999 <47190510+Karandeep1999@users.noreply.github.com>
2020-10-07 08:07:55 -0400

    Add files via upload

---
+++ Forms/MMform.php
@@ -0,0 +1,247 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head> 
+  <link rel="stylesheet" type="text/css" href = "../CSS/form.css">
+    <title>Marketing Materials Request Form</title>
+</head>
+<form  method="post" action="MMform_Database.php"   onsubmit="return ValidateForm(this)";>
+<script type="text/javascript">
+function ValidateForm(frm) {
+if (frm.Requester_Name.value == "") { alert(' Requesters name is required.'); frm.Requester_Name.focus(); return false; }
+if (frm.Marketing_Segment.value == "") { alert('Marketing Segment is required.'); frm.Marketing_Segment.focus(); return false; }
+//if (frm.Company_Name.value == "") { alert('Company/Group Name is required.'); frm.Company_Name.focus(); return false; }
+//if (frm.Target_Code.value == "") { alert('Target Code is required.'); frm.Target_Code.focus(); return false; }
+if (frm.Email_Address.value == "") { alert('Email address is required.'); frm.Email_Address.focus(); return false; }
+if (frm.Email_Address.value.indexOf("@") < 1 || frm.Email_Address.value.indexOf(".") < 1) { alert('Please enter a valid email address.'); frm.Email_Address.focus(); return false; }
+return true; }
+</script>
+<!--
+<table border="1" cellpadding="5" cellspacing="1">
+<tr>
+				<td id="column_heading" colspan="1"  style="text-align: left;">         Business Contact Information       </td>  
+			</tr></table>-->
+
+    <table border="1" cellpadding="5" cellspacing="1">
+                
+	<tr>
+	<td style="width: 50%">
+	<label for="Requester_Name">
+	<b>Requester Name</b></label>
+	<input name="Requester_Name" type="text" maxlength="250" style="width: 260px" />
+	</td>
+
+	<td style="width: 50%">
+	<label for="Market_Segment">
+	<b>Market Segment</b></label>
+	<input name="Market_Segment" type="text" maxlength="250" style="width: 260px" />
+	</td>
+</tr> 
+                <tr>
+	<td style="width: 50%">
+	<label for="Sales_Territory">
+	<b>Sales Territory</b></label>
+	<input name="Sales_Territory" type="text" maxlength="250" style="width: 260px" />
+	</td>
+        	<td style="width: 50%">
+	<label for="Email">
+	<b>Email</b></label>
+	<input name="Email" type="text" maxlength="250" style="width: 260px" />
+	</td>
+        </tr>
+
+                <tr>
+	
+        	<td style="width: 50%">
+	<label for="Phone">
+	<b>Phone</b></label>
+	<input name="Phone" type="text" maxlength="250" style="width: 260px" />
+	</td>
+
+        	<td style="width: 50%">
+	<label for="Date">
+	<b>Today's Date</b></label>
+	<input name="Date" type="text" maxlength="250" style="width: 260px" />
+	</td> </tr>
+    </table>
+    <table border="1" cellpadding="5" cellspacing="1">
+<tr>
+	
+    <td id="column_heading" colspan="2" border="0" style="text-align: left;"><b>Project Background</b></td>
+			</tr></table>
+    <table border="1" cellpadding="5" cellspacing="0">
+<tr> <td colspan = "2">
+<label for="Name_of_Project"><b>Name of Project or Piece </b></label>
+<input name="Name_of_Project" type="text" maxlength="500" style="width: 535px" />
+</td>  
+ </tr>
+<tr> <td border="1">
+   <label for="type_of_project"></label> <b>Type of Project or Piece</b><br>if known<br>If multiple communication are needed,choose all that apply.
+    <input name="type_of_project">
+           </td>
+    <td><table><tr><td border="1">
+<input type="checkbox"  name="type_of_project[]" value="brochure">
+  <label for="brochure">Brochure</label>
+        </td><td>
+    <input type="checkbox"  name="type_of_project[]" value="ppt">
+  <label for="ppt">PowerPoint Presentation</label>
+        </td></tr><tr><td>
+    <input type="checkbox"  name="type_of_project[]" value="Fact_Sheet">
+  <label for="Fact_Sheet">Fact Sheet</label>
+    </td><td>
+    <input type="checkbox"  name="type_of_project[]" value="video">
+  <label for="video">Video</label>
+    </td></tr><tr><td>
+    <input type="checkbox"  name="type_of_project[]" value="mail">
+  <label for="mail">Direct Mail</label>
+    </td><td>
+    <input type="checkbox"  name="type_of_project[]" value="web">
+  <label for="web">Web(specify)</label>
+    
+    <input type="checkbox"  name="type_of_project[]" value="Page">
+    <i><label for="Page">Page</label></i>
+    
+    <input type="checkbox"  name="type_of_project[]" value="Section">
+    <i><label for="Section">Section</label></i>
+    
+    <input type="checkbox"  name="type_of_project[]" value="Blog">
+    <i><label for="Blog">Blog</label></i>
+    
+    <input type="checkbox"  name="type_of_project[]" value="Landing_Page">
+    <i><label for="Landing_Page">Landing Page</label></i>
+    
+    <input type="checkbox"  name="type_of_project[]" value="Update">
+    <i><label for="Update">Update</label></i>
+    
+    <input type="checkbox"  name="type_of_project[]" value="graphic">
+    <i><label for="graphic">Graphic</label></i>
+    </td></tr><tr><td>
+    <input type="checkbox"  name="type_of_project[]" value="Tradeshow">
+    <label for="Tradeshow">Tradeshow</label>
+    </td><td>
+    <input type="checkbox"  name="type_of_project[]" value="item">
+    <label for="item">Promotional item/Giveaway</label>
+    </td></tr><tr><td>
+    <input type="checkbox"  name="type_of_project[]" value="print">
+    <label for="print">Print Aid</label>
+    </td><td>
+    <input type="checkbox"  name="type_of_project[]" value="other">
+    <label for="other">Other (Please specify)</label>
+    </td></tr><tr><td>
+    <input type="checkbox"  name="type_of_project[]" value="release">
+    <label for="release">Press Release/E-blast</label>
+        </td>  <td><textarea name="type_of_project" rows="1" column="100"></textarea></td></tr>
+      
+        
+        
+        
+        </table></td>
+    
+    <tr> <td border="1">
+            <label for="project_content"><b>Is this project:</b></label></td>
+            <input name="project_content">
+            <td><table><tr>
+                <input type="checkbox" id="new" name="project_content[]" value="new">
+                <label for="new">New</label></tr><br/>
+                <tr><input type="checkbox" id="update" name="project_content[]" value="update">
+                <label for="update_info">Update from a previous piece.<br/>If updated from a previous piece,provide the title, reference number, or webpage link below:<br/>
+                <textarea name="update_info" rows="1" column="500"></textarea></label></tr></table>
+        </td></tr></table>
+    <table border="1" cellpadding="5" cellspacing="1">
+<tr>
+	
+    <td id="column_heading" colspan="2" border="0" style="text-align: left;"><b>Target Audiences:</b></td>
+			</tr></table>
+    
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr> <td border="1">
+    <label for="target"><b>Choose all that apply</b>
+        </label>
+            <input name="target"></td>
+    <td><table><tr><td border="1">
+<input type="checkbox" id="prospective_customers" name="target[]" value="prospective_customers">
+  <label for="prospective_customers">Prospective Customers</label>
+        </td><td>
+    <input type="checkbox" id="Engineers" name="target[]" value="Engineers">
+  <label for="Engineers">Engineers</label>
+        </td></tr><tr><td>
+    <input type="checkbox" id="buyers" name="target[]" value="buyers">
+  <label for="buyers">Procurement Managers/Buyers</label>
+    </td><td>
+    <input type="checkbox" id="current" name="target[]" value="current">
+  <label for="current">Current Customers</label>
+    </td></tr><tr><td>
+    <input type="checkbox" id="plant_managers" name="tagret[]" value="plant_managers">
+  <label for="plant_managers">Plant/MRO Managers</label>
+    </td>
+    <td> <input type="checkbox" id="other" name="target[]" value="other">
+    <label for="Other">Other (Please specify)</label><textarea name="target" rows="1" column="100"></textarea></td></tr>
+        </table></td></tr>
+        
+         <tr> <td border="1">
+    <label for="Info"><b>Audience Persona Information</b><br/><ul>
+        <li>Personal Demographics</li>
+        <li>Level of Seniority</li>
+        <li>Pain points/Challenges</li>
+        <li>Goals and Values</li>
+        <li>Questions they ask</li>
+        <li>Where they get information</li>
+        <li>Expected buying Experience</li>
+        <li>Common Objections</li>
+        </ul>
+        </label>
+             </td>
+             <td><textarea name="Info" rows="11" width="1000px" cols="70"></textarea></td></tr>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    <table cellpadding="5" cellspacing="1">
+<tr id="column_heading" colspan="2" border="0" style="text-align: left;">
+	
+    <!--<td id="column_heading" colspan="2" border="0" style="text-align: left;">--><b>Purpose</b><br/>
+			</tr>
+        <tr><td><textarea name="purpose" rows="6" cols="120"></textarea></td></tr></table>
+        
+        <table cellpadding="5" cellspacing="1">
+<tr id="column_heading" colspan="2" border="0" style="text-align: left;">
+        <b>Key Messages</b><br/>
+     
+			</tr>
+        <tr><td><textarea name="key_messages" rows="6" cols="120"></textarea></td></tr></table>
+         <table cellpadding="5" cellspacing="1">
+        <tr id="column_heading" colspan="2" border="0" style="text-align: left;">
+        <b>Supporting Information:</b><br/>
+			</tr>
+        <tr><td><textarea name="support" rows="6" cols="120"></textarea></td></tr></table>
+     <table cellpadding="5" cellspacing="1">
+       <tr id="column_heading" colspan="2" border="0" style="text-align: left;">
+        <b>Photography Needed?</b> 
+    <input type="checkbox"  name="photography[]" value="yes">
+    <i><label for="photography">yes</label></i>
+    
+    <input type="checkbox" name="photography[]" value="no">
+    <i><label for="photography">no</label></i><br/>
+			</tr>
+        <tr><td ><textarea name="photography" rows="6" cols="120"></textarea></td></tr></table>
+        
+        <table border="1" style="text-align: left;"><tr><td ><b>Estimated Quantity:</b></td> <td ><textarea name="estimate" rows="6" cols="40"></textarea></td> <td><i>If applicable.</i></td> </tr>
+        
+        <tr><td ><b>Means of Delivery:</b></td> <td ><textarea name="delivery" rows="6" cols="40"></textarea></td> <td>Anticipated plan for delivering the piece, tradiotional mailing, blogging,<br/> e-mailing, handing out of events, etc.</td></tr>
+            
+           <tr><td ><b>Date Needed:</b></td> <td ><textarea name="date_needed" rows="6" cols="40"></textarea></td> <td>A minimum of 4-8 weeks may be required for many printed materials requests.<br/> The scope of some requests, especially new projects or items to be mailed, may require more time.</td></tr>  
+              <tr><td ><b>Available Budget:</b></td> <td ><textarea name="budget" rows="6" cols="40"></textarea></td> <td>To cover printing, photography or other vendor charges.</td></tr> 
+            
+            
+              <tr><td ><b>Cost Counter #:</b></td> <td ><textarea name="cost" rows="6" cols="40"></textarea></td> <td>If applicable</td></tr> 
+      
+			<tr>
+				<td><input type="Submit" name="Submit" value="Submit"></td>
+				<td><input type="reset" value="Reset"></td>
+			</tr>
+		</table>
+</form>
+</html>
---
+++ Forms/MMform_Database.php
@@ -0,0 +1,88 @@
+<?php
+    if (!session_id()){
+        session_start();
+       //include '../Database/connect.php';
+    }
+
+    $DB_HOST = "localhost";
+    $DB_USER = "root";
+    $DB_PASSWORD = "";
+    $DB_DATABASE = "nylene";
+
+    $connect = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD,$DB_DATABASE);
+if($connect){
+    if(isset($_POST['Submit'])){
+  
+$Requester_Name=$_POST['Requester_Name'];
+$Market_Segment=$_POST['Market_Segment'];
+$Sales_Territory=$_POST['Sales_Territory'];
+$Email=$_POST['Email'];
+$Phone=$_POST['Phone'];
+$Date=$_POST['Date'];
+$Name_of_Project=$_POST['Name_of_Project'];
+$type_of_project=$_POST['type_of_project'];
+//$ppt=$_POST['ppt'];
+//$Fact_Sheet=$_POST['Fact_Sheet'];
+//$video=$_POST['video'];
+//$mail=$_POST['mail'];
+//$web=$_POST['web'];
+  //  $Page=$_POST['Page'];
+//    $Section=$_POST['Section'];
+  //  $Blog=$_POST['Blog'];
+    //$Landing_Page=$_POST['Landing_Page'];
+    //$Update=$_POST['Update'];
+    //$graphic=$_POST['graphic'];
+    //$Tradeshow=$_POST['Tradeshow'];
+    //$item=$_POST['item'];
+    //$print=$_POST['print'];
+    //$other=$_POST['other'];
+    //$release=$_POST['release'];
+    //$other_type=$_POST['other_type'];
+    $project_content=$_POST['project_content'];
+    //$new=$_POST['new'];
+  //  $update=$_POST['update'];
+    $update_info=$_POST['update_info'];
+    $target=$_POST['target'];
+   // $prospective_customers=$_POST['prospective_customers'];
+    //$Engineers=$_POST['Engineers'];
+    //$buyers=$_POST['buyers'];
+    //$current=$_POST['current'];
+    //$plant_managers=$_POST['plant_managers'];
+    //$Other=$_POST['Other'];
+    //$customer_type=$_POST['customer_type'];
+    $Info=$_POST['Info'];
+    //$info_text=$_POST['info_text'];
+   // $photography=$_POST['photography'];
+    $purpose=$_POST['purpose'];
+    $key_messages=$_POST['key_messages'];
+    $support=$_POST['support'];
+    $photography=$_POST['photography'];
+    $estimate=$_POST['estimate'];
+    $delivery=$_POST['delivery'];
+    $date_needed=$_POST['date_needed'];
+    $budget=$_POST['budget'];
+    $cost=$_POST['cost'];
+
+
+ $db=mysqli_select_db($connect,$DB_DATABASE);
+    //echo '<h1>Connected to MySQL</h1>';
+//for($i=0;$i<sizeof ($type_of_project);$i++){
+    //$query=mysqli_query($connect,"INSERT INTO marketing_request_form(type_of_project) VALUES ('$type_of_project')");
+//}
+    $query=mysqli_query($connect,"INSERT INTO marketing_request_form(Requester_Name, market_segment, sales_territory,email,phone,request_date, name_project_or_piece,type_of_project, is_project_new, if_piece_new, target_audiance, audiance_personal_info, purpose, key_message, supporting_info, is_photography_needed, estimated_quantity, means_of_delivery, date_needed, available_budget, cost_center_number ) VALUES ('$Requester_Name', '$Market_Segment', '$Sales_Territory','$Email','$Phone','$Date', '$Name_of_Project','$type_of_project','$project_content','$update_info','$target','$Info','$purpose','$key_messages,'$support','$photography','$estimate','$delivery','$date_needed','$budget','$cost')");
+	if ($query) {
+			header('Location: ../Home/Homepage.php');
+
+	}
+	else{ 
+        echo (mysqli_error($connect)) ;
+		echo "           fail";
+    }
+
+}else{ echo "failed post";}}//
+else{
+    echo ("failed;");
+}
+
+
+?>
