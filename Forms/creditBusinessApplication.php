cf523cd4a21c48cf2414da2e9853dbab5192fd29
kaitlynbreker <52720569+kaitlynbreker@users.noreply.github.com>
2020-11-15 14:47:32 -0500

    fixed a bug for order_pending checkbox  - changed it to a radio and added value to it
    still needs to be fixed on the viewForm to display the value

--- Forms/creditBusinessApplication.php
+++ Forms/creditBusinessApplication.php
@@ -87,8 +87,11 @@ if ($conn->connect_error) {
 			<tr>
 				<td id="fax">Fax</td>
 				<td colspan="2"><input type="text" name="fax" required></td>
-				<td id="order_pending">Order Pending?<input type="checkbox">Yes<input
-					type="checkbox">No
+				<td>Order Pending?
+					<input type="radio" name="order_pending" value="1">
+					<label for="1"> Yes </label>
+					<input type="radio" name="order_pending" value="0">
+					<label for="0"> No </label>
 				</td>
 				<td colspan="2" id="order_amount">Order Amount $<input type="text"
 					name="order_amount"> /lbs.
