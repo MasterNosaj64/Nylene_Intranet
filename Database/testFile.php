adaf8c3f127996548168f3aeffe0948537de9063
waid0005 <waid0005@algonquinlive.com>
2020-11-16 22:33:10 -0500

    TCPDF library with test file
    Doesn't work

--- Database/testFile.php
+++ Database/testFile.php
@@ -1,50 +1,120 @@
+<form method="post" action=testFile.php name="convertToPdf">
+	<table>
+
+		<tr>
+			<td>Form Type</td>
+			<td><select id="selection" required name="formType">
+					<option value="0"></option>
+					<option value="6">Credit Business Application</option>
+					<option value="4">Distributor Quote</option>
+					<option value="2">Light Truckload Quote</option>
+					<option value="5">Marketing Request</option>
+					<option value="1">Sample Request</option>
+					<option value="3">Truckload Quote</option>
+			</select></td>
+		</tr>
+		<tr>
+			<td>Form ID:</td>
+			<td><input type="text" name="formID" /></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="submit" /></td>
+		</tr>
+
+
+	</table>
+</form>
+
 <?php
-include 'interaction.php';
-include 'company.php';
-include 'listBuffer.php';
-include 'connect.php';
-include 'Employee.php';
-include 'customer.php';
+// Include the main TCPDF library (search for installation path).
+require_once('../TCPDF/examples/tcpdf_include.php');
 
-$interaction_Conn = getDBConnection();
-
-
-
-/* $interactions = new Interaction($interaction_Conn);
-
-$interactionData = $interactions->read(); */
-
-
-$companies = new Company($interaction_Conn);
-
-$companyData = $companies->read();
-
-$buffer = create_Buffer($companyData, $companies);
-
-echo "<h1>unsorted</h1><br>";
-
-for ($buffer->rewind(); $buffer->valid(); $buffer->next()) {
+if(isset($_POST['submit'])){
+    //https://tcpdf.org/examples/example_001/
+    // create new PDF document
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     
-    $currentInteractionNode = unserialize($buffer->current());
-   // $conn = getDBConnection();
-    //$customer = new Company($conn);
-   // $customer->searchById($currentInteractionNode->getCustomerId());
-    echo $currentInteractionNode->getName();
-    echo "<br>";
-   // $conn->close();
+    // set document information
+    $pdf->SetCreator(PDF_CREATOR);
+    
+    $pdf->SetAuthor('Nicola Asuni');
+    $pdf->SetTitle('TCPDF Example 001');
+    $pdf->SetSubject('TCPDF Tutorial');
+    $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+    
+    // set default header data
+    $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+    $pdf->setFooterData(array(0,64,0), array(0,64,128));
+    
+    // set header and footer fonts
+    $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+    $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+    
+    // set default monospaced font
+    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    
+    // set margins
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+    
+    // set auto page breaks
+    $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+    
+    // set image scale factor
+    $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+    
+    // set some language-dependent strings (optional)
+    /* if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+        require_once(dirname(__FILE__).'/lang/eng.php');
+        $pdf->setLanguageArray($l);
+    } */
+    
+    // ---------------------------------------------------------
+    
+    // set default font subsetting mode
+    $pdf->setFontSubsetting(true);
+    
+    // Set font
+    // dejavusans is a UTF-8 Unicode font, if you only need to
+    // print standard ASCII chars, you can use core fonts like
+    // helvetica or times to reduce file size.
+    $pdf->SetFont('dejavusans', '', 14, '', true);
+    
+    // Add a page
+    // This method has several options, check the source code documentation for more information.
+    $pdf->AddPage();
+    
+    // set text shadow effect
+    $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+    
+    // Set some content to print
+    $html = <<<EOD
+<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+<i>This is the first example of TCPDF library.</i>
+<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+<p>Please check the source code documentation and other examples for further information.</p>
+<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+EOD;
+    
+    // Print text using writeHTMLCell()
+    $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+    
+    // ---------------------------------------------------------
+    
+    // Close and output PDF document
+    // This method has several options, check the source code documentation for more information.
+    $pdf->Output('example_001.pdf', 'I');
+    
+    //============================================================+
+    // END OF FILE
+    //============================================================+
 }
 
-echo "<h1>sorted?</h1><br>";
 
-$buffer = sortDESC_Name($buffer);
+?>
 
-for ($buffer->rewind(); $buffer->valid(); $buffer->next()) {
-    
-    $currentInteractionNode = unserialize($buffer->current());
-   // $conn = getDBConnection();
-   // $customer = new Company($conn);
-   // $customer->searchById($currentInteractionNode->getCustomerId());
-    echo $currentInteractionNode->getName();
-    echo "<br>";
-    //$conn->close();
-}
+
+
+
+
--- Home/Homepage.php
+++ Home/Homepage.php
@@ -4,9 +4,10 @@ session_start();
 unset($_SESSION['company_id']);
 unset($_SESSION['interaction_id']);
 include '../NavPanel/navigation.php';
+echo "<a href='../Database/testFile.php'>testfile</a>";
 include '../Calendar/calendar.php';
-
 ?>
+
 <html>
 <body>
 
