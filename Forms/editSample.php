2bb818719a52d8f9c9680aec24d3777d4ad9c32a
Madhav00786 <46637049+Madhav00786@users.noreply.github.com>
2020-11-20 17:18:35 -0500

    Add files via upload

---
+++ Forms/editSample.php
@@ -0,0 +1,327 @@
+<?php
+    /* Name: sample_form_view.php
+     * Author: Emmett Janssens, Modified by Kaitlyn Breker
+     * Last Modified: November 12th, 2020
+     * Purpose: Displays the information from the Sample form
+     */
+
+    session_start();
+    include '../NavPanel/navigation.php';
+    include "../Database/connect.php";
+
+    $conn = getDBConnection();
+    
+    /*Check the connection*/
+    if (mysqli_connect_error())
+    {
+		die ('Connect Error (' . mysqli_connect_errno() . ')' . mysqli_connect_error);
+    }
+    else
+    {
+        /*Selection statement for employee that created the form*/
+		$username = "SELECT first_name, last_name FROM employee
+                        INNER JOIN interaction ON interaction.employee_id = employee.employee_id
+                            INNER JOIN interaction_relational_form ON interaction_relational_form.interaction_id = interaction.interaction_id
+                                INNER JOIN sample_form ON sample_form.sample_form_id = interaction_relational_form.form_id
+                                    WHERE sample_form_id = " . $_POST['id'];
+		$result = $conn->query($username);
+		$row = mysqli_fetch_array($result);
+
+    }
+
+    /*Selection statement for form*/
+	$query_samples	= "SELECT * FROM sample_form WHERE sample_form_id = " . $_POST['id'];
+	$query_samples_results = $conn->query($query_samples);
+	$qsr = mysqli_fetch_array($query_samples_results);
+	
+	$_SESSION['field']=$qsr['sample_form_id'];
+
+	/*Selection statement for customer*/
+	$getCustomer = "SELECT * FROM company
+						INNER JOIN company_relational_customer ON company_relational_customer.company_id = company.company_id
+						INNER JOIN customer ON customer.customer_id = company_relational_customer.customer_id WHERE customer.customer_id = " . $qsr['customer_code'];
+
+	$getCustomerResult = $conn->query($getCustomer);
+	$gcr = mysqli_fetch_array($getCustomerResult);
+?>
+
+<html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="../CSS/form.css">
+	</head>
+	<body height="100%" width="100%">
+        	<form name="sample_form" action="editSampleDatabase.php" method="POST" onsubmit="return validateForm()">
+
+            <table class= "form-table" border=1 cellspacing="0" cellpadding="3" align="center">
+            <thead>
+                 <tr>
+                    <th colspan="7" align="center">
+                        Business Contact Information
+                    </th>
+                </tr></thead>
+                <tr>
+                    <td id="info"> Submitted By: </td>
+    
+                    <td colspan="2">  <input name="submittedBy" type="text" readonly value="<?php echo $row['first_name'] . " " . $row['last_name']; ?>"> </td>
+    
+                    <td id="info"> Date Created: </td>
+                    <td> <input name="dateSubmitted" type="text" readonly value="<?php echo $qsr['date_submitted']; ?>"> </td>
+                    <td><label for="m_code">
+                        Market Code:</label><br /> <input
+					name="m_code" placeholder="<?php echo $qsr['m_code']; ?>"/></td>
+    
+    
+                   
+                </tr>
+                <tr>
+                    <td id="info"> Company: </td>
+    
+                    <td colspan="6"><label for="company_name">
+                   </label>
+    				 <input
+					name="company_name" placeholder="<?php echo $gcr['company_name']; ?>"/>
+    					
+    				</td>
+                </tr>
+                <tr>
+                    <td id="info"> Company Address: </td>
+                    <td colspan="3"><label for="company_address">
+                   </label>
+    				 <input
+					name="company_address" placeholder=" <?php echo $gcr['billing_address_street'] . ", " . $gcr['billing_address_city'] . ", " .  $gcr['billing_address_state'] . ", " . $gcr['billing_address_postalcode'] ?>"> </td>
+                    <td id="info"> Primary Contact: </td>
+                    <td colspan="1"> <label for="customer_name">
+                   </label>
+    				 <input
+					name="customer_name" placeholder="<?php echo $gcr['customer_name'] ?> "> </div> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Phone Number:
+                    <td colspan="3"> <label for="customer_phone">
+                   </label>
+    				 <input
+					name="customer_phone" placeholder="<?php echo $gcr['customer_phone'] ?>"> </td>
+                    <td id="info"> Email Address: </td>
+                    <td colspan="1"><label for="customer_email">
+                   </label>
+    				 <input
+					name="customer_email" placeholder=" <?php echo $gcr['customer_email'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Fax Number: </td>
+                    <td colspan="3"><label for="customer_fax">
+                   </label>
+    				 <input
+					name="customer_fax" placeholder=" <?php echo $gcr['customer_fax'] ?>"> </td>
+                    <td id="info"> Credit Application Submitted: </td>
+                    <td colspan="1"> <label for="credit_app_submitted">
+                   </label>
+    				 <input type="checkbox" name="credit_app_submitted" value="1" <?php if($qsr['credit_app_submitted'] == 1) {echo "checked";} ?>> </td>
+                </tr>
+                <thead>
+                <tr>
+                    <th colspan="4" align="center">
+                        - Business Case for Sample -
+                    </th>
+                    <th colspan="2" align="center">
+                        - Match To -
+                    </th>
+                </tr></thead>
+                <tr>
+                    <td colspan="4" rowspan="3" style="height:80px;"> 
+    				 <input
+					name="business_case" placeholder="<?php echo $qsr['business_case'] ?> "></td>
+                    <td colspan="2"> <input type="checkbox" name="match_sample_sub" value=1 <?php if($qsr['match_sample_sub'] == 1) {echo "checked";} ?>> Sample Submission  </td>
+                </tr>
+                <tr>
+                    <td colspan="2"> <input type="checkbox" name="match_data_sheet" value=1<?php if($qsr['match_data_sheet'] == 1) {echo "checked";} ?>> Data Sheet  </td>
+                </tr>
+                <tr>
+                    <td colspan="2"> <input type="checkbox" name="match_descr" value=1 <?php if($qsr['match_description'] == 1) {echo "checked";} ?>> Description  </td>
+                </tr>
+                <thead>
+                <tr>
+                    <th colspan="6" align="center"> - Material Description, Special Handling or Label Request - </th>
+                </tr></thead>
+                <tr>
+                    <td colspan="6"> 
+    				 <input
+					name="company_name" placeholder="<?php echo $qsr['material_description']; if ($qsr['material_description'] == "") {echo "<br/>";}?>"> </td>
+                </tr>
+                <thead>
+                <tr>
+                    <th colspan="6" align="center"> - Additional Information - </th>
+                </tr></thead>
+                <tr>
+                    <td id="info"> Customer Process </td>
+                    <td colspan="2"><label for="customer_proc">
+                   </label>
+    				 <input
+					name="customer_proc" placeholder=" <?php echo $qsr['customer_proc'] ?>"> </td>
+                    <td id="info"> Current Supplier </td>
+                    <td colspan="2"> <label for="customer_supplier">
+                   </label>
+    				 <input
+					name="customer_supplier" placeholder="<?php echo $qsr['customer_supplier'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Finished Good Application </td>
+                    <td colspan="2"> <label for="finished_good_app">
+                   </label>
+    				 <input
+					name="finished_good_app" placeholder="<?php echo $qsr['finished_good_app'] ?>"> </td>
+                    <td id="info"> Est. Annual Volume </td>
+                    <td colspan="2"> <label for="annual_vol">
+                   </label>
+    				 <input
+					name="annual_vol" placeholder="<?php echo $qsr['annual_vol'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Current Base Resin System </td>
+                    <td colspan="2"><label for="current_resin_system">
+                   </label>
+    				 <input
+					name="current_resin_system" placeholder=" <?php echo $qsr['current_resin_system'] ?>"> </td>
+                    <td id="info"> Target Price </td>
+                    <td colspan="2"> <label for="target_price">
+                   </label>
+    				 <input
+					name="target_price" placeholder="<?php echo $qsr['target_price'] ?> "></td>
+                </tr>
+                <tr>
+                    <td id="info"> Melt Requirements </td>
+                    <td colspan="5"><label for="melt_reqs">
+                   </label>
+    				 <input
+					name="melt_reqs" placeholder=" <?php echo $qsr['melt_reqs'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Current Filler System </td>
+                    <td colspan="5"><label for="current_filler_sys">
+                   </label>
+    				 <input
+					name="current_filler_sys" placeholder=" <?php echo $qsr['current_filler_sys'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Color(s) </td>
+                    <td colspan="5"><label for="colors">
+                   </label>
+    				 <input
+					name="colors" placeholder=" <?php echo $qsr['colors'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Known Additive Packages </td>
+                    <td colspan="5"><label for="known_additives">
+                   </label>
+    				 <input
+					name="known_additives" placeholder=" <?php echo $qsr['known_additives'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> UV Requirements </td>
+                    <td colspan="5"> <label for="uv_reqs">
+                   </label>
+    				 <input
+					name="uv_reqs" placeholder="<?php echo $qsr['uv_reqs'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> UL Requirements </td>
+                    <td colspan="5"><label for="ul_reqs">
+                   </label>
+    				 <input
+					name="ul_reqs" placeholder=" <?php echo $qsr['ul_reqs'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Automotive Specifications </td>
+                    <td colspan="5"><label for="auto_reqs">
+                   </label>
+    				 <input
+					name="auto_reqs" placeholder=" <?php echo $qsr['auto_reqs'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> FDA Requirements </td>
+                    <td colspan="5"> <label for="fda_reqs">
+                   </label>
+    				 <input
+					name="fda_reqs" placeholder="<?php echo $qsr['fda_reqs'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td id="info"> Color Specifications </td>
+                    <td colspan="5"><label for="color_specs">
+                   </label>
+    				 <input
+					name="color_specs" placeholder=" <?php echo $qsr['color_specs'] ?>"> </td>
+                </tr>
+                <thead>
+                <tr>
+                    <th colspan="6" align="center"> - Type Of Response Needed By - <?php echo $qsr['response_date'] ?> </th>
+                </tr></thead>
+                <tr>
+                    <td> <input type="checkbox" name="prod_rec" value=1 <?php if($qsr['prod_rec'] == 1) {echo "checked";} ?>> Product Recommendation  </td>
+                    <td> <input type="checkbox" name="stock_prod_qty", value=1 <?php if($qsr['stock_prod_qty'] == 1) {echo "checked";} ?>>Stock Product QTY  </td>
+                    <td colspan="2" id="info" > Other Documentation (Specify) </td>
+                    <td colspan="2"><label for="other_doc">
+                   </label>
+    				 <input
+					name="other_doc" placeholder=" <?php echo $qsr['other_doc'] ?>">  </td>
+                </tr>
+                <tr>
+                    <td> <input type="checkbox" name="sds" value=1 <?php if($qsr['sds'] == 1) {echo "checked";} ?>> SDS  </td>
+                    <td> <input type="checkbox" name="coa" value=1 <?php if($qsr['coa'] == 1) {echo "checked";} ?>> COA  </td>
+                    <td colspan="2" id="info"> Sample Quantity </td>
+                    <td> QTY:<label for="sample_qty">
+                   </label>
+    				 <input
+					name="sample_qty" placeholder=" <?php echo $qsr['sample_qty'] ?>"> </td>
+                    <td> Date REQ:<label for="sample_req_date">
+                   </label>
+    				 <input
+					name="sample_req_date" placeholder=" <?php echo $qsr['sample_req_date'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td colspan="2"> <input type="checkbox" name="data_sheet" value=1 <?php if($qsr['data_sheet'] == 1) {echo "checked";} ?>> Data Sheet  </td>
+                    <td colspan="2" id="info"> Charge/No Charge </td>
+                    <td> Price:<label for="sample_price">
+                   </label>
+    				 <input
+					name="sample_price" placeholder=" <?php echo $qsr['sample_price'] ?>"> </td>
+                    <td> Frt PDD/Add:<label for="sample_frt">
+                   </label>
+    				 <input
+					name="sample_frt" placeholder=" <?php echo $qsr['sample_frt'] ?>"> </td>
+                </tr>
+                <tr>
+                    <td colspan="6" id="info" align="center"> ---Note: SDS Sent With All Samples---</td>
+                </tr>
+                <thead>
+                <tr>
+                    <th colspan="6" align="center"> - Distribution List - </th>
+                </tr></thead>
+                <tr>
+                    <td colspan="3" style="width:50%">Other Contact 1: <label for="other_contact_1">
+                   </label>
+    				 <input
+					name="other_contact_1" placeholder="<?php echo $qsr['other_contact_1']; if ($qsr['other_contact_1'] == "") {echo "<br/>";} ?>"> </td>
+    				<td colspan="3" style="width:50%"> Other Contact 2: <label for="other_contact_2">
+                   </label>
+    				 <input
+					name="other_contact_2" placeholder="<?php echo $qsr['other_contact_2'] ?>"> </td>
+    			</tr>
+    			<tr>
+                    <td colspan="3" style="width:50%">Other Contact 3:  <label for="other_contact_3">
+                   </label>
+    				 <input
+					name="other_contact_3" placeholder="<?php echo $qsr['other_contact_3']; if ($qsr['other_contact_3'] == "") {echo "<br/>";} ?> "></td>
+    				<td colspan="3" style="width:50%">Other Contact 4: <label for="other_contact_4">
+                   </label>
+    				 <input
+					name="other_contact_4" placeholder=" <?php echo $qsr['other_contact_4'] ?> "></td>
+    			</tr>
+				<tr>
+                    <td colspan="3"> <input type="submit" style="width:100%"> </td>
+                    <td colspan="3"> <input type="reset" style="width:100%"> </td>
+                </tr>
+            </table>
+    	</form>
+         
+	</body>
+</html>
---
+++ Forms/editSampleDatabase.php
@@ -0,0 +1,264 @@
+<?php
+    /* Name: newSampleForm.php
+     * Author: Emmett Janssens, Modified by Kaitlyn Breker
+     * Last Modified: November 15th, 2020
+     * Purpose: File called when user clicks submit on the input sample form. Inserts form information into
+     *          the sample_form table of the database.
+     */
+	
+   	if (!session_id()) {
+ session_start();}Q
+include '../Database/databaseConnection.php';
+include '../Database/connect.php';
+			
+			$field=$_SESSION['field'];
+			
+			
+			
+			
+	$query_samples	= "SELECT * FROM sample_form WHERE sample_form_id = " . $field;
+	$query_samples_results = $conn->query($query_samples);
+	$qsr = mysqli_fetch_array($query_samples_results);
+	
+	//$_SESSION['field']=$qsr[sample_form_id];
+    /*Check required variables for value, if none input 0*/
+	if (isset($_POST['credit_app_submitted']))
+	{
+		$credit_app_submitted = $_POST['credit_app_submitted'];
+		
+	}
+	else 
+	{
+		$credit_app_submitted = $qsr['credit_app_submitted'];
+	}
+
+	if (isset($_POST['business_case']))
+	{
+		$match_sample_sub = $_POST['business_case'];
+	}
+	else 
+	{
+		$match_sample_sub =$qsr['business_case'] ;
+	}
+	if (isset($_POST['match_sample_sub']))
+	{
+		$match_data_sheet = $_POST['match_sample_sub'];
+	}
+	else 
+	{
+		$match_data_sheet = $qsr['match_saC1_sub'];
+	}
+
+	if (isset($_POST['match_data_sheet']))
+	{
+		$match_data_sheet = $_POST['match_data_sheet'];
+	}
+	else 
+	{
+		$match_data_sheet = $qsr['match_data_sheet'];
+	}
+	if (isset($_POST['match_descr']))
+	{
+		$match_descr = $_POST['match_descr'];
+	}
+	else 
+	{
+		$match_descr = $qsr['match_descr'];
+	}
+	if (isset($_POST['curr_filter_sys']))
+	{
+		$curr_filter_sys = $_POST['curr_filter_sys'];
+	}
+	else 
+	{
+		$curr_filter_sys = $qsr['curr_filter_sys'];
+	}
+	if (isset($_POST['prod_rec']))
+	{
+		$prod_rec = $_POST['prod_rec'];
+	}
+	else 
+	{
+		$prod_rec = $qsr['prod_rec'];
+	}
+
+	if (isset($_POST['stock_prod_qty']))
+	{
+		$stock_prod_qty = $_POST['stock_prod_qty'];
+	}
+	else 
+	{
+		$stock_prod_qty = $qsr['stock_prod_qty'];
+	}
+	
+	if (isset($_POST['sds']))
+	{
+		$sds = $_POST['sds'];
+	}
+	else 
+	{
+		$sds = $qsr['sds'];
+	}
+	if (isset($_POST['coa']))
+	{
+		$coa = $_POST['coa'];
+	}
+	else 
+	{
+		$coa = $qsr['coa'];
+	}
+	if (isset($_POST['data_sheet']))
+	{
+		$data_sheet = $_POST['data_sheet'];
+	}
+	else 
+	{
+		$data_sheet = $qsr['data_sheet'];
+	}
+  
+	/*Check submittedBy field, if blank, display error*/
+    if (empty($_POST['submittedBy']))
+    {
+        echo "There were some errors with your form:";
+        $referer = filter_var($_SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL);
+	
+        if (!empty($referer)) 
+        {
+		    echo '<p><a href="'. $referer .'" title="Return to the previous page">&laquo; Go back</a></p>';
+        } 
+        else 
+        {
+		    echo '<p><a href="javascript:history.go(-1)" title="Return to the previous page">&laquo; Go back</a></p>';	
+	  }
+	}
+  
+	/*Assign values to variables*/
+	$submittedBy		= filter_input(INPUT_POST, 'submittedBy');
+	$dateSubmitted		= htmlspecialchars(strip_tags($_POST['dateSubmitted']));
+	$marketCode			= htmlspecialchars(strip_tags($_POST['mCode']));
+	$customer_id        = htmlspecialchars(strip_tags($_SESSION['customer_id']));
+	$company_id         = htmlspecialchars(strip_tags($_SESSION['company_id']));
+	$business_case		= htmlspecialchars(strip_tags($_POST['business_case']));
+	$material_descr		= htmlspecialchars(strip_tags($_POST['material_descr']));
+	$customer_proc		= htmlspecialchars(strip_tags($_POST['customer_proc']));
+	$curr_supplier		= htmlspecialchars(strip_tags($_POST['curr_supplier']));
+	$finished_good_app	= htmlspecialchars(strip_tags($_POST['finised_good_app']));
+	$annual_vol			= htmlspecialchars(strip_tags($_POST['annual_vol']));
+	$curr_resin_system	= htmlspecialchars(strip_tags($_POST['curr_resin_system']));
+	$target_price		= htmlspecialchars(strip_tags($_POST['target_price']));
+	$melt_reqs			= htmlspecialchars(strip_tags($_POST['melt_reqs']));
+	$colors				= htmlspecialchars(strip_tags($_POST['colors']));
+	$known_additives	= htmlspecialchars(strip_tags($_POST['known_additives']));
+	$uv_reqs			= htmlspecialchars(strip_tags($_POST['uv_reqs']));
+	$ul_reqs			= htmlspecialchars(strip_tags($_POST['ul_reqs']));
+	$auto_reqs			= htmlspecialchars(strip_tags($_POST['auto_reqs']));
+	$fda_reqs			= htmlspecialchars(strip_tags($_POST['fda_reqs']));
+	$color_specs		= htmlspecialchars(strip_tags($_POST['color_specs']));
+	$response_date		= htmlspecialchars(strip_tags($_POST['response_date']));
+	$other_doc			= htmlspecialchars(strip_tags($_POST['other_doc']));
+	$sample_qty			= htmlspecialchars(strip_tags($_POST['sample_qty']));
+	$sample_req_date	= htmlspecialchars(strip_tags($_POST['sample_req_date']));
+	$sample_price		= htmlspecialchars(strip_tags($_POST['sample_price']));
+	$sample_frt			= htmlspecialchars(strip_tags($_POST['sample_frt']));
+	$other_contact1		= htmlspecialchars(strip_tags($_POST['other_contact1']));
+	$other_contact2		= htmlspecialchars(strip_tags($_POST['other_contact2']));
+	$other_contact3		= htmlspecialchars(strip_tags($_POST['other_contact3'])); 
+	$other_contact4		= htmlspecialchars(strip_tags($_POST['other_contact4']));
+
+	include "../Database/connect.php";
+
+	$conn = getDBConnection();
+	
+	
+	/*Check the connection*/
+	if (mysqli_connect_error())
+	{
+		die('Connect Error ('. mysqli_connect_errno() . ') '.mysqli_connect_error);
+	}
+	else 
+	{
+        /*Prepare insert statement into the sample_form table*/
+        $stmt = $conn->prepare("INSERT INTO sample_form (
+					date_submitted, 
+                    m_code, 
+                    customer_code, 
+                    credit_app_submitted, 
+                    business_case, 
+                    match_sample_sub,
+				    match_data_sheet, 
+                    match_description, 
+                    material_description, 
+                    customer_proc, 
+                    customer_supplier, 
+                    finished_good_app,
+				    annual_vol, 
+                    current_resin_system, 
+                    target_price, 
+                    melt_reqs, 
+                    current_filler_sys, 
+                    colors, 
+                    known_additives,
+				    uv_reqs, 
+                    ul_reqs, 
+                    auto_reqs, 
+                    fda_reqs, 
+                    color_specs, 
+                    response_date, 
+                    prod_rec, 
+                    stock_prod_qty,
+				    sds, 
+                    coa, 
+                    data_sheet, 
+                    other_doc, 
+                    sample_qty, 
+                    sample_req_date, 
+                    sample_price, 
+                    sample_frt,
+				    other_contact_1, 
+                    other_contact_2, 
+                    other_contact_3, 
+                    other_contact_4) 
+                values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		
+        /*Bind statement parameters to statement and execute*/
+		$stmt->bind_param("sssisiiissssssssssssssssiiisiisiissssss", $dateSubmitted, $marketCode, $customer_id, $credit_app_submitted, $business_case, $match_sample_sub,
+		    $match_data_sheet, $match_descr, $material_descr, $customer_proc, $curr_supplier, $finished_good_app,
+		    $annual_vol, $curr_resin_system, $target_price, $melt_reqs, $curr_filter_sys, $colors, $known_additives,
+		    $uv_reqs, $ul_reqs, $auto_reqs, $fda_reqs, $color_specs, $response_date, $prod_rec, $stock_prod_qty,
+		    $sds, $coa, $data_sheet, $other_doc, $sample_qty, $sample_req_date, $sample_price, $sample_frt,
+		    $other_contact1, $other_contact2, $other_contact3, $other_contact4);
+		
+		$stmt->execute();
+
+		/*Modified by Jason, to take Interaction_id generated previously*/
+		$id = $_SESSION['interaction_id'];
+		
+		/*Prepare insert statement into the interaction_relational_form table*/
+		$stmt2 = $conn->prepare("INSERT INTO interaction_relational_form (
+					interaction_id,
+                    form_id,
+                    form_type)
+                    VALUES (?, ?, ?)");
+	
+		/*Assign values to variables*/
+		$interactionNum = $id;
+		$formID = $conn->insert_id; //retrieve id of last query under $conn
+		$formType = 1;
+
+		/*Bind statement parameters to statement*/
+		$stmt2->bind_param("iii", $interactionNum, $formID, $formType);
+		
+		/*Execute statement*/
+		$stmt2->execute();
+		
+		/*Close statements and connection*/
+		$stmt->close();
+		$stmt2->close();
+		$conn->close();
+	  
+		echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
+		exit();
+
+	}
+?>
+
