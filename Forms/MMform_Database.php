ce870060c0b17a59d4a50f8454a3ba66318d03f7
Karandeep1999 <47190510+Karandeep1999@users.noreply.github.com>
2020-11-21 10:13:54 -0500

    Add files via upload

--- Forms/MMform_Database.php
+++ Forms/MMform_Database.php
@@ -273,103 +273,65 @@ if ($conn->connect_error) {
     $budget = htmlspecialchars(strip_tags($_POST['budget']));
     $cost = htmlspecialchars(strip_tags($_POST['cost']));
     
-    if($stmt!== FALSE){
+  //  if($stmt!== FALSE){
             
      $stmt->bind_param("ssssssssssssssssssssssssssssssssssssssssssss", $Requester_Name, $Market_Segment, $Sales_Territory, $Email, $Phone, $Date, $Name_of_Project, $type_of_project,$other_type_of_project, $brochure, $ppt, $fact_sheet, $video, $direct_mail, $web, $page, $section, $blog, $landing_page, $updt, $graphic, $tradeshow, $promotional_item, $print_aid, $press_release, $project_content, $update_info, $prospective_customers, $engineers, $procurement_managers, $current_customers, $plant_managers,$other_audience, $Info, $purpose,  $key_messages, $support, $is_photography_needed,  $needed_photography, $estimate, $delivery, $date_needed, $budget, $cost);
     
      $stmt -> execute();
     
+    /*
      $getFormId = "SELECT marketing_request_id FROM marketing_request_form ORDER BY marketing_request_id DESC";
         $formId = $conn->query($getFormId);
         $id_form = mysqli_fetch_array($formId);
     }else{
         die('prepare() failed: ' . htmlspecialchars($conn->error));
     }
-    
+    */
      $stmt2 = $conn->prepare("INSERT INTO interaction_relational_form (
                                      interaction_id,
                                      form_id, form_type)
                                       VALUES (?,?,?)");
-
-        $stmt2 -> bind_param("iii", $interactionNum, $formId, $formType);
     
     $interactionNum = $interaction_id;
-        $formId = $id_form['marketing_request_id'];
+        $formId = $conn-> insert_id;
         $formType = 5;
+        
+        $stmt2 -> bind_param("iii", $interactionNum, $formId, $formType);
         
         $stmt2 -> execute();
      
         $stmt->close();
         $stmt2->close();
-        $conn->close();
+    
+         /*Search follow up info using interaction id posted from session value*/
+		$interactionQuery = "SELECT status, follow_up_type FROM interaction
+								WHERE interaction_id = ". $interactionNum;
+		$interactionResult = $conn->query($interactionQuery);
+		$interactionRow = mysqli_fetch_array($interactionResult);
         
-        echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
+        /*Code for updating date in interaction table if form selected*/
+		if (($interactionRow['status'] == 'open') && ($interactionRow['follow_up_type'] == 'form')){
+		    /*Prepare Update statement into the interaction table to update notification date*/
+		    $stmt3 = $conn->prepare("UPDATE interaction SET follow_up_date = ?
+                                        WHERE interaction_id = ?");
+            $fDate = strtotime($Date);
+		    $followDate = date("Y/m/d", $fDate);
+		    $followUpDate = date_create($followDate);
+		    date_modify($followUpDate, "+30 days");
+		    $followUpDateFormatted = date_format($followUpDate,"Y/m/d");
+            
+             $stmt3->bind_param("si", $followUpDateFormatted, $interactionNum);
+		    
+		    /*Execute statement*/
+		    $stmt3->execute();
+		    $stmt3->close();
+        }else{
+            
+        };
+        $conn->close();
+       echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
         exit();
+    
    }
 
 ?>
-</html>
-/*
-if ($conn) {
-    if (isset($_POST['Submit'])) {
-        $interaction_id = $_SESSION['interaction_id'];
-        $Requester_Name = $_POST['Requester_Name'];
-        $Market_Segment = $_POST['Market_Segment'];
-        $Sales_Territory = $_POST['Sales_Territory'];
-        $Email = $_POST['Email'];
-        $Phone = $_POST['Phone'];
-        $Date = $_POST['Date'];
-        $Name_of_Project = $_POST['Name_of_Project'];
-        $type_of_project = $_POST['type_of_project'];
-        $project_content = $_POST['project_content'];
-        $update_info = $_POST['update_info'];
-        $target = $_POST['target'];
-        $Info = $_POST['Info'];
-        $purpose = $_POST['purpose'];
-        $key_messages = $_POST['key_messages'];
-        $support = $_POST['support'];
-        $is_photography_needed = $_POST['is_photography_needed'];
-        $photography_needed = $_POST['needed_photography'];
-        $estimate = $_POST['estimate'];
-        $delivery = $_POST['delivery'];
-        $date_needed = $_POST['date_needed'];
-        $budget = $_POST['budget'];
-        $cost = $_POST['cost'];
-
-        /*
-         * if(isset($_POST['type_of_project'])){
-         * $t1=implode(',', $_POST['type_of_project']);
-         */
-    }
-    // $in_ch=mysqli_query($conn,"insert into marketing_request_form(type_of_project) values ('$t1')");
-
-    $db = mysqli_select_db($conn, $dbname);
-
-    $query = mysqli_query($conn, "INSERT INTO marketing_request_form(requester_name,market_segment, sales_territory,email,phone,request_date,
-                                  name_project_or_piece,is_project_new, if_piece_new, target_audiance, audiance_personal_info, purpose, key_message, 
-                                  supporting_info, is_photography_needed, needed_photography, estimated_quantity, means_of_delivery, date_needed, 
-                                  available_budget, cost_center_number ) 
-                                  VALUES ('$Requester_Name','$Market_Segment', '$Sales_Territory','$Email','$Phone','$Date', '$Name_of_Project','$project_content',
-                                  '$update_info','$target','$Info','$purpose','$key_messages','$support','$is_photography_needed','$needed_photography','$estimate',
-                                  '$delivery','$date_needed','$budget','$cost')");
-
-    $getFormId = "SELECT marketing_request_id FROM marketing_request_form ORDER BY marketing_request_id DESC";
-    $formId = $conn->query($getFormId);
-    $id_form = mysqli_fetch_array($formId);
-
-    $insert_into_interaction_relational_manager_table = "INSERT INTO interaction_relational_form (
-					interaction_id, form_id, form_type) values ('$interaction_id', " . $id_form['marketing_request_id'] . ", '5')";
-
-    if ($conn->query($insert_into_interaction_relational_manager_table) === TRUE) {
-        echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
-        exit();
-    } else {
-        echo "Error: " . $conn->error;
-    }
-}
-$conn->close();
-else{ echo "failed post";}//
-('$Requester_Name', '$Market_Segment', '$Sales_Territory', '$Email', '$Phone', '$Date', '$Name_of_Project', '$type_of_project', '$brochure', '$ppt', '$fact_sheet', '$video', '$direct_mail', '$web', '$page', '$section', '$blog', '$landing_page', '$update', '$graphic', '$tradeshow', '$promotional_item', '$print_aid', '$press_release', '$project_content', '$update_info', '$prospective_customers', '$engineers', '$procurement_managers', '$current_customers', '$plant_managers', '$Info', '$purpose',  '$key_messages', '$support',  '$needed_photography', '$estimate', '$delivery', '$date_needed', '$budget', '$cost'
-)
-?>
-*/
