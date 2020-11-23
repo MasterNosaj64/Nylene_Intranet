2b5dcd7ffb1a02507df611c7643d6209e820d894
waid0005 <waid0005@algonquinlive.com>
2020-11-22 20:38:15 -0500

    updated testing interface

--- Database/jasonTestFile.php
+++ Database/jasonTestFile.php
@@ -1,11 +1,53 @@
 <?php
 if (isset($_POST['submit'])) {
 
-    if ($_POST['formType'] == 3) {
-
-        echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/tlQuoteForm_TCPDF.php?id={$_POST['formID']}\" />";
-        exit();
+    
+    
+    switch($_POST['formType']){
+        
+        //Sample Request
+        case 1:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/sampleForm_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;
+            //Light Truckload Quote
+        case 2:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/ltlQuoteForm_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;
+            //tlquote
+        case 3:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/tlQuoteForm_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;
+            //Distributor Quote
+        case 4:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/distributorQuoteForm_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;
+            //Marketing Request
+        case 5:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/marketingRequestForm_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;   
+            //Credit Business Application
+        case 6:
+            
+            echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Nylene_TCPDF_Forms/creditBusinessApp_TCPDF.php?id={$_POST['formID']}\" />";
+            exit();
+            break;
+            //none
+        default:
+            echo "Select a form type to convert to pdf";
+            
+            break;
     }
+   
 }
 
 ?>
@@ -29,7 +71,7 @@ if (isset($_POST['submit'])) {
 		</tr>
 		<tr>
 			<td>Form ID:</td>
-			<td><input type="text" name="formID" /></td>
+			<td><input type="text" required name="formID" /></td>
 		</tr>
 		<tr>
 			<td><input type="submit" name="submit" value="Submit" /></td>
