f088025781eb194dfb5e95aea354ffb2e518bd60
Madhav00786 <46637049+Madhav00786@users.noreply.github.com>
2020-11-25 07:11:28 -0500

    Add files via upload

--- NavPanel/navigation.php
+++ NavPanel/navigation.php
@@ -10,46 +10,47 @@ include '../admin/sessionController.php';
 ?>
 
 <html>
-
-<head>
-<link rel="stylesheet" href="../CSS/nav.css" />
-<link rel="icon" href="../favicon.png">
-</head>
 <div>
-	<div class="navigation">
+	<head>
+<link rel="stylesheet" href="../CSS/nav.css">
 
-		<!-- 	<a href="homepage.php">Nylene</a> -->
+<div class="navigation">
 
-		<a href="../Home/homepage.php"><span title="Home"><img width="180"
-				align="middle" heigth="auto" src="../Graphics/Nylene-alt.png"></span></a>
+	<!-- 	<a href="homepage.php">Nylene</a> -->
 
-		<!--  <a href="sample.php">Sample</a> -->
+	<a href="../Home/homepage.php"><span title="Home"><img width="180"
+			align="middle" heigth="auto" src="../Graphics/Nylene-alt.png"></span></a>
+
+	<!--  <a href="sample.php">Sample</a> -->
 
  <?php //$image_url='Nylene.png'; ?>
 
 <!--   <a href="#employee">Employee</a> -->
 
 <?php
-if ($_SESSION['role'] == "admin") {
 
-    echo "<div class='dropdown-user'>";
-    echo "<button class='dropbtn'>Admin Panel";
-    echo "</button>";
-    echo "<div class='dropdown-content'>";
 
-    if (strcmp(basename($_SERVER['PHP_SELF']), "createUser.php")) {
-        echo "<a href='../Admin/createUser.php'>Create User</a>";
-    }
-    echo "<a href='../Admin/editUserDatabase.php'>Edit User</a>";
-    echo "</div>";
-    echo "</div>";
+if ($_SESSION['role'] == "admin" || $_SESSION['role'] == "supervisor") {
+
+	echo "<div class='dropdown-user'>";
+	echo "<button class='dropbtn'>Admin Panel";
+	echo "</button>";
+	echo "<div class='dropdown-content'>";
+
+	if (strcmp(basename($_SERVER['PHP_SELF']), "createUser.php")) {
+		echo "<a href='../Admin/createUser.php'>Create User</a>";
+	}
+	echo "<a href='../Admin/editUserDatabase.php'>Edit User</a>";
+	echo "</div>";
+	echo "</div>";
 }
+
 ?>
 
 
   <div class="dropdown-user">
-			<button class="dropbtn">Company Directory</button>
-			<div class="dropdown-content">
+		<button class="dropbtn">Company Directory</button>
+		<div class="dropdown-content">
     <?php
 
     // hides searchCompany button if user is already on the searchCompany.php page
@@ -62,7 +63,7 @@ if ($_SESSION['role'] == "admin") {
     }
     ?>
 		</div>
-		</div>
+	</div>
 
 <?php
 if (isset($_SESSION['company_id'])) {
@@ -86,32 +87,36 @@ if (isset($_SESSION['interaction_id']) || isset($_POST['interaction_id']) || iss
 
 
 <!--   <div class="dropdown"> -->
-		<!--     <button class="dropbtn">Clients -->
-		<!--     </button> -->
-		<!--     <div class="dropdown-content"> -->
-		<!--       <a href="#">Search Client</a> -->
-		<!--       <a href="#">Add Client</a> -->
-		<!--     </div> -->
-		<!--   </div> -->
+	<!--     <button class="dropbtn">Clients -->
+	<!--     </button> -->
+	<!--     <div class="dropdown-content"> -->
+	<!--       <a href="#">Search Client</a> -->
+	<!--       <a href="#">Add Client</a> -->
+	<!--     </div> -->
+	<!--   </div> -->
 
-		<!--   <a href="InteractionManager.php">interaction Manager</a> -->
+	<!--   <a href="InteractionManager.php">interaction Manager</a> -->
 
 
-		<div class="dropdown-user">
+	<div class="dropdown-user">
 
-			<button class="dropbtn">
-				<img src="../Graphics/userIcon.png" width="20" heigth="auto"
-					align="middle"> <?php echo $_SESSION['name'];?>
+		<button class="dropbtn">
+			<img src="../Graphics/userIcon.png" width="20" heigth="auto"
+				align="middle"> <?php echo $_SESSION['name'];?>
     </button>
-			<div class="dropdown-content">
-				<a href="../logout.php">Log Out</a>
-				<!--  -->
+		<div class="dropdown-content">
+			<a href="../Admin/viewProfile.php">View Profile</a>
+			<a href="../logout.php">Log Out</a>
 
-			</div>
+			
+			
+			<!--  -->
+
 		</div>
-
-
 	</div>
+
+
+</div>
 
 </div>
 </html>
