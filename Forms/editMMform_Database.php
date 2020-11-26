1306dee4a9fdc8f3a5edd393747b81b1b7dc70bb
Karandeep1999 <47190510+Karandeep1999@users.noreply.github.com>
2020-11-26 10:38:38 -0500

    Add files via upload

--- Forms/editMMform_Database.php
+++ Forms/editMMform_Database.php
@@ -7,101 +7,13 @@
 //include '../Database/databaseConnection.php';
 include '../Database/connect.php';
 			
-			$field=$_SESSION['field'];
-
-$query_marketing	= "SELECT * FROM marketing_request_form WHERE marketing_request_id = " . $field;
-	$query_marketing_results = $conn->query($query_marketing);
-	$row = mysqli_fetch_array($query_marketing_results);
-    
-
-
-if (isset($_POST['requester_name']))
-	{
-		$Requester_Name = $_POST['requester_name'];
-		
-	}
-	else 
-	{
-		$Requester_Name = $row['requester_name'];
-	}
-
-if (isset($_POST['market_segment']))
-{
-		$Market_Segment = $_POST['market_segment'];
-		
-	}
-	else 
-	{
-		$Market_Segment = $row['market_segment'];
-	}
-	
-if (isset($_POST['sales_territory']))
-{
-		$Sales_Territory = $_POST['sales_territory'];
-		
-	}
-	else 
-	{
-		$Sales_Territory = $row['sales_territory'];
-	}
-
-if (isset($_POST['email']))
-{
-		$Email = $_POST['email'];
-		
-	}
-	else 
-	{
-		$Email = $row['email'];
-	}
-
-if (isset($_POST['phone']))
-{
-		$Phone = $_POST['phone'];
-		
-	}
-	else 
-	{
-		$Phone = $row['phone'];
-	}
-
-if (isset($_POST['request_date']))
-{
-		$Date = $_POST['request_date'];
-		
-	}
-	else 
-	{
-		$Date = $row['request_date'];
-	}
-
-if (isset($_POST['name_project_or_piece']))
-{
-		$Name_of_Project = $_POST['name_project_or_piece'];
-		
-	}
-	else 
-	{
-		$Name_of_Project = $row['name_project_or_piece'];
-	}
-
-if (isset($_POST['type_of_project']))
-{
-		$type_of_project = $_POST['type_of_project'];
-		
-	}
-	else 
-	{
-		$type_of_project = $row['type_of_project'];
-	}
-
-if (isset($_POST['brochure']))
+ if (isset($_POST['brochure']))
 	{
 		$brochure = $_POST['brochure'];
 	}
 	else 
 	{
-		$brochure = $row['brochure'];
+		$brochure = 0;
 	}
     
     if (isset($_POST['ppt']))
@@ -110,7 +22,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$ppt = $row['ppt'];
+		$ppt = 0;
 	}
     
     if (isset($_POST['fact_sheet']))
@@ -119,7 +31,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$fact_sheet = $row['fact_sheet'];
+		$fact_sheet = 0;
 	}
     
     if (isset($_POST['video']))
@@ -128,7 +40,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$video = $row['video'];
+		$video = 0;
 	}
     
     if (isset($_POST['direct_mail']))
@@ -137,7 +49,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$direct_mail = $row['direct_mail'];
+		$direct_mail = 0;
 	}
     
     if (isset($_POST['web']))
@@ -146,7 +58,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$web = $row['web'];
+		$web = 0;
 	}
     
     if (isset($_POST['page']))
@@ -155,7 +67,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$page =$row['page'];
+		$page = 0;
 	}
     
     if (isset($_POST['section']))
@@ -164,7 +76,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$section = $row['section'];
+		$section = 0;
 	}
     
     if (isset($_POST['blog']))
@@ -173,7 +85,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$blog = $row['blog'];
+		$blog = 0;
 	}
     
     if (isset($_POST['landing_page']))
@@ -182,7 +94,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$landing_page = $row['landing_page'];
+		$landing_page = 0;
 	}
     
     if (isset($_POST['updt']))
@@ -191,7 +103,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$updt = $row['updt'];
+		$updt = 0;
 	}
     
     if (isset($_POST['graphic']))
@@ -200,7 +112,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$graphic = $row['graphic'];
+		$graphic = 0;
 	}
     
     if (isset($_POST['tradeshow']))
@@ -209,7 +121,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$tradeshow = $row['tradeshow'];
+		$tradeshow = 0;
 	}
     
     if (isset($_POST['promotional_item']))
@@ -218,7 +130,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$promotional_item = $row['promotional_item'];
+		$promotional_item = 0;
 	}
     
     if (isset($_POST['print_aid']))
@@ -227,7 +139,7 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$print_aid = $row['print_aid'];
+		$print_aid = 0;
 	}
     if (isset($_POST['press_release']))
 	{
@@ -235,43 +147,16 @@ if (isset($_POST['brochure']))
 	}
 	else 
 	{
-		$press_release = $row['press_release'];
+		$press_release = 0;
 	}
-
-if (isset($_POST['other_type_of_project']))
-	{
-		$other_type_of_project = $_POST['other_type_of_project'];
-	}
-	else 
-	{
-		$other_type_of_project = $row['other_type_of_project'];
-	}
-
-if (isset($_POST['is_project_new']))
-	{
-		$project_content = $_POST['is_project_new'];
-	}
-	else 
-	{
-		$project_content = $row['is_project_new'];
-	}
-
-if (isset($_POST['if_piece_new']))
-	{
-		$update_info = $_POST['if_piece_new'];
-	}
-	else 
-	{
-		$update_info = $row['if_piece_new'];
-	}
-
-if (isset($_POST['prospective_customers']))
+    
+    if (isset($_POST['prospective_customers']))
 	{
 		$prospective_customers = $_POST['prospective_customers'];
 	}
 	else 
 	{
-		$prospective_customers = $row['prospective_customers'];
+		$prospective_customers = 0;
 	}
     
     if (isset($_POST['engineers']))
@@ -280,7 +165,7 @@ if (isset($_POST['prospective_customers']))
 	}
 	else 
 	{
-		$engineers = $row['engineers'];
+		$engineers = 0;
 	}
     
     if (isset($_POST['procurement_managers']))
@@ -289,7 +174,7 @@ if (isset($_POST['prospective_customers']))
 	}
 	else 
 	{
-		$procurement_managers = $row['procurement_managers'];
+		$procurement_managers = 0;
 	}
     
     if (isset($_POST['current_customers']))
@@ -298,7 +183,7 @@ if (isset($_POST['prospective_customers']))
 	}
 	else 
 	{
-		$current_customers = $row['current_customers'];
+		$current_customers = 0;
 	}
     
     if (isset($_POST['plant_managers']))
@@ -307,115 +192,7 @@ if (isset($_POST['prospective_customers']))
 	}
 	else 
 	{
-		$plant_managers = $row['plant_managers'];
-	}
-if (isset($_POST['other_audience']))
-	{
-		$other_audience = $_POST['other_audience'];
-	}
-	else 
-	{
-		$other_audience = $row['other_audience'];
-	}
-
-if (isset($_POST['audiance_personal_info']))
-	{
-		$Info = $_POST['audiance_personal_info'];
-	}
-	else 
-	{
-		$Info = $row['audiance_personal_info'];
-	}
-
-if (isset($_POST['purpose']))
-	{
-		$purpose = $_POST['purpose'];
-	}
-	else 
-	{
-		$purpose = $row['purpose'];
-	}
-
-if (isset($_POST['key_message']))
-	{
-		$key_messages = $_POST['key_message'];
-	}
-	else 
-	{
-		$key_messages = $row['key_message'];
-	}
-
-if (isset($_POST['supporting_info']))
-	{
-		$support = $_POST['supporting_info'];
-	}
-	else 
-	{
-		$support = $row['supporting_info'];
-	}
-
-if (isset($_POST['is_photography_needed']))
-	{
-		$is_photography_needed = $_POST['is_photography_needed'];
-	}
-	else 
-	{
-		$is_photography_needed = $row['is_photography_needed'];
-	}
-
-if (isset($_POST['needed_photography']))
-	{
-		$needed_photography = $_POST['needed_photography'];
-	}
-	else 
-	{
-		$needeed_photography = $row['needed_photography'];
-	}
-
-if (isset($_POST['estimated_quantity']))
-	{
-		$estimate = $_POST['estimated_quantity'];
-	}
-	else 
-	{
-		$estimate = $row['estimated_quantity'];
-	}
-
-
-if (isset($_POST['means_of_delivery']))
-	{
-		$delivery = $_POST['means_of_delivery'];
-	}
-	else 
-	{
-		$delivery = $row['means_of_delivery'];
-	}
-
-if (isset($_POST['date_needed']))
-	{
-		$date_needed = $_POST['date_needed'];
-	}
-	else 
-	{
-		$date_needed = $row['date_needed'];
-	}
-
-if (isset($_POST['available_budget']))
-	{
-		$budget = $_POST['available_budget'];
-	}
-	else 
-	{
-		$budget = $row['available_budget'];
-	}
-
-if (isset($_POST['cost_center_number']))
-	{
-		$cost = $_POST['cost_center_number'];
-	}
-	else 
-	{
-		$cost = $row['cost_center_number'];
+		$plant_managers = 0;
 	}
     
 $conn = getDBConnection();
@@ -425,7 +202,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     
-    $interaction_id = $_SESSION['interaction_id'];
+   // $interaction_id = $_SESSION['interaction_id'];
     $stmt = $conn->prepare("UPDATE marketing_request_form SET
                                                             requester_name= ?,
                                                             market_segment= ?, 
@@ -471,9 +248,9 @@ if ($conn->connect_error) {
                                                             date_needed= ?,
                                                             available_budget= ?,
                                                             cost_center_number= ? 
-                                                            WHERE marketing_request_id = " .$field);
+                                                            WHERE marketing_request_id = ?" );
                                                 
-   /* 
+   
     $Requester_Name = htmlspecialchars(strip_tags($_POST['Requester_Name']));
     $Market_Segment = htmlspecialchars(strip_tags($_POST['Market_Segment']));
     $Sales_Territory = htmlspecialchars(strip_tags($_POST['Sales_Territory']));
@@ -496,74 +273,22 @@ if ($conn->connect_error) {
     $delivery = htmlspecialchars(strip_tags($_POST['delivery']));
     $date_needed = htmlspecialchars(strip_tags($_POST['date_needed']));
     $budget = htmlspecialchars(strip_tags($_POST['budget']));
-    $cost = htmlspecialchars(strip_tags($_POST['cost']));*/
+    $cost = htmlspecialchars(strip_tags($_POST['cost']));
+    $marketing_request_id = htmlspecialchars(strip_tags($_POST['marketing_request_id']));
     
     if($stmt!== FALSE){
             
-     $stmt->bind_param("sssssssssssssssssssssssssssssssssssssssssssss", $Requester_Name, $Market_Segment, $Sales_Territory, $Email, $Phone, $Date, $Name_of_Project, $type_of_project,$other_type_of_project, $brochure, $ppt, $fact_sheet, $video, $direct_mail, $web, $page, $section, $blog, $landing_page, $updt, $graphic, $tradeshow, $promotional_item, $print_aid, $press_release, $project_content, $update_info, $prospective_customers, $engineers, $procurement_managers, $current_customers, $plant_managers,$other_audience, $Info, $purpose,  $key_messages, $support, $is_photography_needed,  $needed_photography, $estimate, $delivery, $date_needed, $budget, $cost);
+     $stmt->bind_param("sssssssssiiiiiiiiiiiiiiiiiiiisiiiiisssssssssi", $Requester_Name, $Market_Segment, $Sales_Territory, $Email, $Phone, $Date, $Name_of_Project, $type_of_project,$other_type_of_project, $brochure, $ppt, $fact_sheet, $video, $direct_mail, $web, $page, $section, $blog, $landing_page, $updt, $graphic, $tradeshow, $promotional_item, $print_aid, $press_release, $project_content, $update_info, $prospective_customers, $engineers, $procurement_managers, $current_customers, $plant_managers,$other_audience, $Info, $purpose,  $key_messages, $support, $is_photography_needed,  $needed_photography, $estimate, $delivery, $date_needed, $budget, $cost, $marketing_request_id);
     
      $stmt -> execute();
-        }else{
-        die('prepare() failed: ' . htmlspecialchars($conn->error));
-    }
-    
-    /*
-     $getFormId = "SELECT marketing_request_id FROM marketing_request_form ORDER BY marketing_request_id DESC";
-        $formId = $conn->query($getFormId);
-        $id_form = mysqli_fetch_array($formId);
-    }else{
-        die('prepare() failed: ' . htmlspecialchars($conn->error));
-    }
-    */
-    
-     $stmt2 = $conn->prepare("INSERT INTO interaction_relational_form (
-                                     interaction_id,
-                                     form_id, form_type)
-                                      VALUES (?,?,?)");
-    
-    $interactionNum = $interaction_id;
-        $formId = $conn-> insert_id;
-        $formType = 5;
-        
-        $stmt2 -> bind_param("iii", $interactionNum, $formId, $formType);
-        
-        $stmt2 -> execute();
-     
-        $stmt->close();
-        $stmt2->close();
-    
-    
-         /*Search follow up info using interaction id posted from session value*/
-    /*
-		$interactionQuery = "SELECT status, follow_up_type FROM interaction
-								WHERE interaction_id = ". $interactionNum;
-		$interactionResult = $conn->query($interactionQuery);
-		$interactionRow = mysqli_fetch_array($interactionResult);
-        */
-        
-        /*Code for updating date in interaction table if form selected*/
-    
-		//if (($interactionRow['status'] == 'open') && ($interactionRow['follow_up_type'] == 'form')){
-		    /*Prepare Update statement into the interaction table to update notification date*/
-		/*    $stmt3 = $conn->prepare("UPDATE interaction SET follow_up_date = ?
-                                        WHERE interaction_id = ?");
-            $fDate = strtotime($Date);
-		    $followDate = date("Y/m/d", $fDate);
-		    $followUpDate = date_create($followDate);
-		    date_modify($followUpDate, "+30 days");
-		    $followUpDateFormatted = date_format($followUpDate,"Y/m/d");
-            
-             $stmt3->bind_param("si", $followUpDateFormatted, $interactionNum);
-		    
-		    
-		    $stmt3->execute();
-		    $stmt3->close();
-        }else{
-            
-        };*/
+          $stmt->close();
+  
         $conn->close();
-      echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
+        echo "<meta http-equiv = \"refresh\" content = \"0; url = ../Interactions/companyHistory.php\" />;";
         exit();
+        }else{
+        die('prepare() failed: ' . htmlspecialchars($conn->error));
+    }
     
    }
 
