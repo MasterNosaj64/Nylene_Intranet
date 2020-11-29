88e47a1527f3e75f4377a4cfee77ee53565a98b3
kaitlynbreker <52720569+kaitlynbreker@users.noreply.github.com>
2020-11-29 14:25:26 -0500

    updated maxlength check for comment appending for ltl form

--- Forms/editLtlQuote_Database.php
+++ Forms/editLtlQuote_Database.php
@@ -197,18 +197,30 @@
         /*If autoUpdate == 1, do changes*/
         if ($autoUpdate == 1){
             
-            //Implement This
-            /*Ensure strlen(comments) does not reach max length of field*/
-            
-            
             $comments = $interactionRow['comments'];
-            $comments .= "\n{$commentString}";
-            $stmt3 = $conn->prepare("UPDATE interaction SET
-                                        comments = ?
-                                        WHERE interaction_id = ?");
-            $stmt3->bind_param("si", $comments, $interactionNum);
-            $stmt3->execute();
-            $stmt3->close();
+            
+            /*Only update the comments in the interaction if the max length is not reached*/
+            $old_commentLength = strlen($comments);
+            if($old_commentLength >= 1024){
+                //echo "Cannot append modified changes to comments, exceeding max length for comments in database";
+            } else {
+                
+                /*Check new comments for length*/
+                $comments .= "\n{$commentString}";
+                $newCommentLength = strlen($comments);
+                
+                if($newCommentLength < 1024){
+                    
+                    /*Update comments in the interaction with the modified fields*/
+                    $stmt3 = $conn->prepare("UPDATE interaction SET comments = ?
+                                                WHERE interaction_id = ?");
+                    $stmt3->bind_param("si", $comments, $interactionNum);
+                    $stmt3->execute();
+                    $stmt3->close();
+                } else {
+                    //echo "Cannot append modified changes to comments, exceeding max length for comments in database";
+                }
+            }
         } else {
             //do nothing
         }
