4aef28d823e26928aadd3152e2609d6a63f8e265
Isha <ishamittal2018@gmail.com>
2020-11-29 20:11:30 -0500

    formatted the code

--- Admin/createUser.php
+++ Admin/createUser.php
@@ -1,24 +1,17 @@
 
-
 <?php
 session_start();
-//TODO: MADHAV change database connection file and align code to mysqli standard
+// TODO: MADHAV change database connection file and align code to mysqli standard
 unset($_SESSION['company_id']);
 unset($_SESSION['interaction_id']);
 include '../NavPanel/navigation.php';
 include '../Database/databaseConnection.php';
 include '../Database/connect.php';
 
-//TODO: MADHAV call getDBConnection to get connection
-//$conn = getDBConnection();
+// TODO: MADHAV call getDBConnection to get connection
+// $conn = getDBConnection();
 
-
-
-
-$accessLevel=$_SESSION['role'];
-
-
-
+$accessLevel = $_SESSION['role'];
 
 ?>
 
@@ -71,73 +64,67 @@ x[1].value = today;
 		<table class="form-table" border="1" cellpadding="5" cellspacing="0">
 
 
-<thead>
-			<tr>
-				<td id="column_heading" colspan="2" style="text-align: center;">Standard
-					Information</td>
-			</tr></thead>
+			<thead>
+				<tr>
+					<td id="column_heading" colspan="2" style="text-align: center;">Standard
+						Information</td>
+				</tr>
+			</thead>
 
 		</table>
 		<table class="form-table" border="1" cellpadding="5" cellspacing="0">
-			
+
 			<tr>
-				<td ><label for="first_name"> First Name *</label>
-					<input name="first_name" type="text" maxlength="50"
-					 /></td>
-				<td style="width: 50%"><label for="last_name">Last Name *</label>
-					<input name="last_name" type="text" maxlength="50"
-					 /></td>
+				<td><label for="first_name"> First Name *</label> <input
+					name="first_name" type="text" maxlength="50" /></td>
+				<td style="width: 50%"><label for="last_name">Last Name *</label> <input
+					name="last_name" type="text" maxlength="50" /></td>
 			</tr>
 			<tr>
-			
-				<td> Title* <select id="title" name="title">
-				
-				<?php 
-				
-				if ($accessLevel==admin){
-					echo '<option style="width: 260px" value=""></option>';
-					echo '<option style="width: 260px" value="admin">Administrator</option>';
-					echo '<option style="width: 260px" value="supervisor">Supervisor</option>';
-					echo '<option style="width: 260px" value="sales_rep">Sales Representative</option>';
-					echo '<option style="width: 260px" value="ind_rep">Independent Representative</option>';
 
-				}
-				else if ($accessLevel==supervisor){
-					echo '<option style="width: 260px" value=""></option>';
-					echo '<option style="width: 260px" value="sales_rep">Sales Representative</option>';
-					echo '<option style="width: 260px" value="ind_rep">Independent Representative</option>';
-				}
-
+				<td>Title* <select id="title" name="title">
 				
-				?>
+				<?php
+
+    if ($accessLevel == admin) {
+        echo '<option style="width: 260px" value=""></option>';
+        echo '<option style="width: 260px" value="admin">Administrator</option>';
+        echo '<option style="width: 260px" value="supervisor">Supervisor</option>';
+        echo '<option style="width: 260px" value="sales_rep">Sales Representative</option>';
+        echo '<option style="width: 260px" value="ind_rep">Independent Representative</option>';
+    } else if ($accessLevel == supervisor) {
+        echo '<option style="width: 260px" value=""></option>';
+        echo '<option style="width: 260px" value="sales_rep">Sales Representative</option>';
+        echo '<option style="width: 260px" value="ind_rep">Independent Representative</option>';
+    }
+
+    ?>
 				
 								<!--		<option value=""></option>
 						<option value="1">Admin</option>
 						<option value="2">Manager</option>
 						<option value="3">Independent/Member</option>
-						--></select> 
-						</td>
-						 
-						
-				<td><label for="department">Department</label> <input
-					name="department" type="text" maxlength="50" />
+						-->
+				</select>
 				</td>
+
+
+				<td><label for="department">Department</label> <input
+					name="department" type="text" maxlength="50" /></td>
 			</tr>
 
 			<tr>
 				<td><label for="work_phone">Work Phone *</label><br /> <input
-					name="work_phone" type="text" maxlength="50" />
-				</td>
+					name="work_phone" type="text" maxlength="50" /></td>
 				<td>Reports to <select id="employee" name="reports_to">
 		<?php
-if ($accessLevel==admin){
-	$sql = "SELECT * FROM employee WHERE title='admin' OR title='supervisor'";
-}
-else if($accessLevel==supervisor){
-	$sql = "SELECT * FROM employee WHERE title='supervisor'";
+if ($accessLevel == admin) {
+    $sql = "SELECT * FROM employee WHERE title='admin' OR title='supervisor'";
+} else if ($accessLevel == supervisor) {
+    $sql = "SELECT * FROM employee WHERE title='supervisor'";
 }
 $query = mysqli_query($conn, $sql);
- echo '<option style="width: 260px" value=""</option>';
+echo '<option style="width: 260px" value=""</option>';
 while ($row = mysqli_fetch_array($query)) {
     echo '<option style="width: 260px" value=' . $row['employee_id'] . '>' . $row['first_name'] . " " . $row['last_name'] . '</option>';
 }
@@ -147,38 +134,37 @@ while ($row = mysqli_fetch_array($query)) {
 
 		</table>
 		<table class="form-table" border="1" cellpadding="5" cellspacing="0">
-		<thead>
-			<tr>
-				<td id="column_heading" colspan="2" style="text-align: center;">Secondary
-					Information</td>
-			</tr> 
+			<thead>
+				<tr>
+					<td id="column_heading" colspan="2" style="text-align: center;">Secondary
+						Information</td>
+				</tr>
 			</thead>
 		</table>
 		<table class="form-table" border="1" cellpadding="5" cellspacing="0">
-		
-			<tr>	<td colspan="2"><label for="username">Username *</label><br /> <input
-					name="username" type="text" />
-				</td>
+
+			<tr>
+				<td colspan="2"><label for="username">Username *</label><br /> <input
+					name="username" type="text" /></td>
 			</tr>
 
 			<tr>
 
-				
+
 				<td colspan="2" style="width: 100%"><label for="Status">Status</label><br />
-					<input name="STATUS" type="text" maxlength="100"/></td>
+					<input name="STATUS" type="text" maxlength="100" /></td>
 
 			</tr>
 
 
 			<tr>
-				<td colspan="2"><label for="employee_email">Email *</label><br />
-					<input name="employee_email" type="text" maxlength="100"
-					 /></td>
+				<td colspan="2"><label for="employee_email">Email *</label><br /> <input
+					name="employee_email" type="text" maxlength="100" /></td>
 			</tr>
 			<tr>
-				<td colspan="2"><label for="password">Password *</label><br />
-					<input type="password" id="password" name="password" type="text" maxlength="100"
-					 /></td>
+				<td colspan="2"><label for="password">Password *</label><br /> <input
+					type="password" id="password" name="password" type="text"
+					maxlength="100" /></td>
 			</tr>
 		</table>
 
